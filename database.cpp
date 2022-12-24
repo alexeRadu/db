2b340cfe6594da0c2531f43e9cdf96446edcab60
Radu Alexe <alexeradu2007@gmail.com>
2022-12-24 10:44:26 +0200

    small changes

--- database.cpp
+++ database.cpp
@@ -14,12 +14,16 @@ static void next_token(char *s, char *token)
 {
 }
 
+    std::cout << "---" << cmdline << "+++" << std::endl;
     if (!strcmp(cmdline, "exit")) {
         return 1;
     } else if (!strcmp(cmdline, "create table")) {
+        std::cout << "CREATE TABLE" << std::endl;
         tables[cmdline] = new Table();
     } else if (!strcmp(cmdline, "drop table")) {
     } else if (!strcmp(cmdline, "display table")) {
+    } else {
+        std::cout << "Unknown comman" << std::endl;
     }
 
     return 0;
